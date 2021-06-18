c71d0d476227428e7cf52aa638076804c6acb1c7
abu-bakarr <abangura14@yahoo.com>
2021-06-18 14:32:03 +0000

    7th exercise

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	int j = 48;
+
+	for (i = 0; i < 10; i++, j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
