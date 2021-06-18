7832ad91d5e0a92e3b337dddfdd4a3b00575e356
abu-bakarr <abangura14@yahoo.com>
2021-06-18 14:37:10 +0000

    10th exercise

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	int i;
+	int j = 48;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(j);
+
+		if (j != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		j++;
+	}
+	putchar('\n');
+	return (0);
+}
