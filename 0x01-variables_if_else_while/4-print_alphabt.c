ed26c2f833c17b61dcce6ecd667f87b3d20e37bb
abu-bakarr <abangura14@yahoo.com>
2021-06-18 14:29:31 +0000

    5th exercise

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	char c = 'a';
+
+	for (i = 0; i < 24; i++)
+	{
+
+		if (c == 'e' || c == 'q')
+		{
+			c++;
+		}
+		putchar(c);
+		c++;
+
+	}
+
+	putchar('\n');
+
+	return (0);
+}
