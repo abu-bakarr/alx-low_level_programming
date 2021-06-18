e93d9b68c7e5ea75d07eeb12cfbb0857e7526109
abu-bakarr <abangura14@yahoo.com>
2021-06-18 14:27:24 +0000

    3rd exercise

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	char c = 'a';
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
