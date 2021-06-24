8e3c9cb053aa21ad7891f1a99a8119a7b082eea0
abu-bakarr <abangura14@yahoo.com>
2021-06-24 09:53:41 +0000

    update

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,16 @@
+
 #include "holberton.h"
 /**
-* _isupper - Short description, single line
-* @c: Description of parameter c
-* Return: 0
-*/
+ * _isupper - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _isupper(int c)
 {
-
-	if (c >= 65  && c <= 90)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
-
-
+if (c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
 }
