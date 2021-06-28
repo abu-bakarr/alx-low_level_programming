db49bbeb1170e89e20162c113db7e8b01775cc08
abu-bakarr <abangura14@yahoo.com>
2021-06-28 08:40:37 +0000

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * puts_half - the second half of the string
+ * @str: a variable string as referencia
+	*
+ * followed by a new line.
+	* Return: none.
+ */
+
+void puts_half(char *str)
+{
+	int len = 0, i = 0, s = 0;
+
+	while (str[len])
+		len++;
+	s = len / 2;
+
+	if (len % 2)
+		s += 1;
+	i = s;
+	for (; i < len; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
