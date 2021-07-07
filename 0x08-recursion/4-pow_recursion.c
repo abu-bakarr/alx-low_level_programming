2918174213f70f6f4d9c4caad683c74ffd085fc2
abu-bakarr <abangura14@yahoo.com>
2021-07-07 10:43:27 +0000

    pow recurs

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,13 +1,22 @@
- * _pow_recursion - returns value of x ^ y.
- * @x: int value
- * @y: power to be raised
- * Return: x * y-1 till y == 0 -> y = 1 and unwindes
- * thus causing x to multiply itself
- */
+#include "holberton.h"
+/**
+* _pow_recursion - Short description, single line
+* @x: Description of parameter n
+* @y: Description of parameter n
+* Return: 0
+*/
 int _pow_recursion(int x, int y)
 {
-	if (y < 0)
+	if (y != 0 && !(y < 0))
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+	else if (y < 0)
+	{
 		return (-1);
-	if (y == 0)
+	}
+	else
+	{
 		return (1);
-	return (x * _pow_recursion(x, y - 1));
+	}
+}
