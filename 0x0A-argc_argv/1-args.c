ca3c9565c38bd35b5057785f81793a3298e71990
abu-bakarr <abangura14@yahoo.com>
2021-07-12 21:11:43 +0000

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - function for this project.
+ * @argc: is the length
+ * @argv: string that holds statement.
+ * Return: Null/Zero.
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", (argc - 1));
+	return (0);
+}
