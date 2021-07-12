d83bbf8c2d83b6ac62ace0cbd0bc0accba02a2dd
abu-bakarr <abangura14@yahoo.com>
2021-07-12 21:19:01 +0000

    advance task

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,52 +1,51 @@
+#include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+
 /**
- * main - prints the minimum number of coins to make change for an amount
- *\of money
- * @argc: count of the arguments
- * @argv: an array of pointers to the strings which are those arguments
+ * main - prints buffer in hexa
+ * @argc: he number of command line arguments
+ * @argv: An array containing the program command line arguments.
  *
- * Return: returns 0 or 1
+ * Return: Nothing.
  */
 int main(int argc, char *argv[])
 {
-	int money, change, qt, dime, nk, two, pn;
+	int j = 0, i = 1, coins, change = 0, a;
+	int cents[5] = {25, 10, 5, 2, 1};
 
-	if (argc != 2)
+	if (argc == 2)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] == '-')
+			{
+				printf("0\n");
+				return (0);
+			}
+			if (!(isdigit(argv[i][j])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		coins = atoi(argv[i]);
+		for (a = 0; a < 5; a++)
+		{
+
+			while (coins >= cents[a])
+			{
+				coins -= cents[a];
+				change += 1;
+			}
+		}
+		printf("%d\n", change);
+	}
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-
-	change = qt = dime = nk = two = pn = 0;
-	money = atoi(argv[1]);
-
-	if (money / 25 != 0)
-	{
-		qt = money / 25;
-		money = money - (qt * 25);
-	}
-	if (money / 10 != 0)
-	{
-		dime = money / 10;
-		money = money - (dime * 10);
-	}
-	if (money / 5 != 0)
-	{
-		nk = money / 5;
-		money = money - (nk * 5);
-	}
-	if (money / 2 != 0)
-	{
-		two = money / 2;
-		money = money - (two * 2);
-	}
-	if (money / 1 != 0)
-	{
-		pn = money;
-	}
-
-	change = qt + dime + nk + two + pn;
-	printf("%d\n", change);
 	return (0);
 }
