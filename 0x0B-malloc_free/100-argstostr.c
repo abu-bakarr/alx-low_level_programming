d63a17e5d4eba942c6560b73dff7acb235b5bd33
abu-bakarr <abangura14@yahoo.com>
2021-07-13 23:13:55 +0000

    advance

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,47 +1,46 @@
+/**
+ * _strlen - return length of string
+ * @s: string being measured
+ * Return: length of string
+ */
 #include "holberton.h"
-#include <stdlib.h>
-#include <stdio.h>
+int _strlen(char *s)
+{
+	int t = 0;
+
+	while (s[t] != '\0')
+	{
+		t += 1;
+	}
+	return (t);
+}
 
 /**
- * argstostr - Function that concatenates arguments
- * @ac: Variable type int
- * @av: Pointer to pointer type char
- * Return: A pointer.
+ * argstostr - concatenates all arguments
+ * @ac: argument counter
+ * @av: argument vector
+ * Return: 0
  */
-
+#include "holberton.h"
 char *argstostr(int ac, char **av)
 {
-	int i = 0, x = 0, len = 0;
-	char *a;
+	int l, m;
+	char *catstr;
+	int start = 0, length = 0;
 
 	if (ac == 0 || av == NULL)
-	{
 		return (NULL);
-	}
-	else
+	for (l = 0; l < ac; l++)
+		length += _strlen(av[l]);
+	catstr = malloc(sizeof(char) * (length + ac + 1));
+	if (catstr == NULL)
+		return (NULL);
+	for (l = 0; l < ac; l++)
 	{
-		for (i = 0, len = 0; i < ac; i++, len++)
-		{
-			for (x = 0; av[i][x] != '\0'; x++, len++)
-				;
-		}
-
-		a = malloc(sizeof(char) * (len + 1));
-
-		if (a == NULL)
-		{
-			return (NULL);
-		}
-		else
-		{
-			for (i = 0, len = 0; i < ac; i++, len++)
-			{
-				for (x = 0; av[i][x] != '\0'; x++, len++)
-					*(a + len) = av[i][x];
-				*(a + len) = '\n';
-			}
-			*(a + len) = '\0';
-			return (a);
-		}
+		for (m = 0; av[l][m] != '\0'; m++)
+			catstr[start++] = av[l][m];
+		catstr[start++] = '\n';
 	}
+	catstr[start] = '\0';
+	return (catstr);
 }
