e423e2d32deeebf12056309d8247a5eefc8e0b77
abu-bakarr <abangura14@yahoo.com>
2021-07-13 23:06:42 +0000

    fifth

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,4 +1,10 @@
 #include "holberton.h"
+/**
+ * free_grid - free allocated 2D array
+ * @grid: grid being freed
+ * @height: height of array
+ */
+
 void free_grid(int **grid, int height)
 {
 	int l;
