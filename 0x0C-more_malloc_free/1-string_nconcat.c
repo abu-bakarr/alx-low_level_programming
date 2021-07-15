f3fb76a693704db903b93c02d52c9b4333d860e4
abu-bakarr <abangura14@yahoo.com>
2021-07-15 16:06:59 +0000

    string concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,53 +1,41 @@
 #include "holberton.h"
-#include <stdlib.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * string_nconcat - Function that concatenates two strings.
- * @s1: Pointer type char
- * @s2: Pointer type char
- * @n: Variable type unsigned int
- * Return: A pointer
- */
-
+ * string_nconcat - void
+ * @s1: string
+ * @s2: string
+ * @n: unsigned integer
+ * Return: p
+*/
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *a;
-	unsigned int i, x, len1 = 0, len2 = 0, flag = 0;
-
-	if (s1 == NULL)
-		s1 = "";
-
-	if (s2 == NULL)
-		s2 = "";
-
-	while (s1[len1] != '\0')
-		len1++;
-
-	while (s2[len2] != '\0')
-		len2++;
-
-	a = malloc(sizeof(char) * (len1 + n + 1));
-
-	if (a == NULL)
-	{
-		return (NULL);
-	}
-	else
-	{
-		if (n >= len2)
-			flag = len2;
-		else
-			flag = n;
-
-		for (i = 0; i < len1; i++)
-			a[i] = s1[i];
-
-		for (x = 0; x < flag; x++, i++)
-			a[i] = s2[x];
-
-		a[i] = '\0';
-
-		return (a);
-	}
+unsigned int j;
+char *p;
+int i = 0;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+while (s1[i] != 0)
+{
+i++;
 }
+p = malloc((i + n + 1) * (sizeof(char)));
+if (p == NULL)
+{
+return (NULL);
+}
+i = j = 0;
+while (s1[i] != 0)
+{
+p[i] = s1[i];
+i++;
+}
+while (j < n && s2[j] != 0)
+{
+p[i] = s2[j];
+i++, j++;
+}
+p[i] = '\0';
+return (p);
