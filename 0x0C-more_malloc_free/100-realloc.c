ee257e01c140fe52a350a410b5cc1197ce85cc02
abu-bakarr <abangura14@yahoo.com>
2021-07-15 16:21:18 +0000

    advance

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,53 +1,43 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
- * _realloc - memory in blocks
- * @ptr: string
- * @old_size: string
- * @new_size: string
- * Return: p
-*/
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+ * _realloc - function that concatenates two strings.
+ * @old_size: number min.
+ * @new_size: number max.
+ * @s: array input.
+ * Return: Always 0.
+ */
+void *_realloc(void *s, unsigned int old_size, unsigned int new_size)
 {
-unsigned char *p;
-unsigned int i = 0;
-if (new_size == old_size)
-{
-return (ptr);
-}
-if (new_size == 0 && ptr != NULL)
-{
-free(ptr);
-return (NULL);
-}
-if (ptr == NULL)
-{
-ptr = malloc(new_size * (sizeof(void *)));
-if (ptr == NULL)
-{
-return (NULL);
-}
-return (ptr);
-}
-p = malloc(new_size * (sizeof(char)));
-if (p == NULL)
-{
-return (NULL);
-}
-if (new_size > old_size)
-{
-for (i = 0; i < old_size; i++)
-{
-p[i] = ((char *)ptr)[i];
-}
-free(ptr);
-return (p);
-}
-for (i = 0; i < new_size; i++)
-{
-p[i] = ((char *)ptr)[i];
-}
-free(ptr);
-return (p);
+	char *str;
+	char *ptr = s;
+	unsigned int i;
+
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	if (ptr == NULL)
+	{
+		str = malloc(new_size);
+		return (str);
+	}
+	str = malloc(new_size);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < old_size; i++)
+	{
+		*(str + i) = *(ptr + i);
+	}
+	free(ptr);
+	return (str);
 }
