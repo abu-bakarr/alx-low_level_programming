6e213c653f221f0dee1db4e4d7f091abeda108f0
abu-bakarr <abangura14@yahoo.com>
2021-07-15 16:10:05 +0000

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,52 +1,22 @@
 #include "holberton.h"
-#include <stdlib.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 /**
- * _calloc - Function that allocates memory for an array with malloc.
- * @nmemb: Variable type unsigned int
- * @size: Variable type unsigned int
- * Return: A pointer
- */
-
+ * _calloc - void
+ * @nmemb: string
+ * @size: string
+ * Return: p
+*/
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *a;
-
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-
-	a = malloc(nmemb * size);
-
-	if (a == NULL)
-	{
-		return (NULL);
-	}
-	else
-	{
-		_memset(a, 0, (nmemb * size));
-
-		return (a);
-	}
-}
-
-/**
- * _memset - Function that fills memory with a constant byte
- * @s: Pointer type char
- * @b: VAriable type char
- * @n: Variable type int
- * Return: A pointer s
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-	int i = 0;
-
-	while (n > 0)
-	{
-		s[i] = b;
-		i++;
-		n--;
-	}
-
-	return (s);
+char *p;
+unsigned int i;
+if (nmemb == 0 || size == 0)
+return (NULL);
+p = malloc(nmemb * size);
+if (p == NULL)
+return (NULL);
+for (i = 0; i < (nmemb * size); i++)
+p[i] = 0;
+return (p);
 }
