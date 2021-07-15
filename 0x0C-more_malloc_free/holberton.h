c0274168e004a5a3ab6d76f09f3ac0c39082427b
abu-bakarr <abangura14@yahoo.com>
2021-07-15 16:22:35 +0000

    advance

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,5 +1,5 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
+#ifndef HOLBERTON_H_FILE
+#define HOLBERTON_H_FILE
 
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
