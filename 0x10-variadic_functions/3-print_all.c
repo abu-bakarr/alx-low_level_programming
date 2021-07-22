58417fa405caf908fc96c40df53b6980daca8bae
abu-bakarr <abangura14@yahoo.com>
2021-07-22 03:49:06 +0000

    print all fixed

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,90 +1,52 @@
 #include "variadic_functions.h"
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
 /**
- * print_char - prints a name as is
- * @p: list
- *
- * Return: Nothing.
- */
-void print_char(va_list p)
-{
-	printf("%c", va_arg(p, int));
-}
-/**
- * print_int - prints a name as is
- * @p: list
- *
- * Return: Nothing.
- */
-void print_int(va_list p)
-{
-	printf("%d", va_arg(p, int));
-}
-/**
- * print_float - prints a name as is
- * @p: list
- *
- * Return: Nothing.
- */
-void print_float(va_list p)
-{
-	printf("%f", va_arg(p, double));
-}
-/**
- * print_string - prints a name as is
- * @p: list
- *
- * Return: Nothing.
- */
-void print_string(va_list p)
-{
-	char *s = va_arg(p, char*);
-
-	if (s == NULL)
-	{
-		printf("(nil)");
-		return;
-	}
-	printf("%s", s);
-}
-/**
- * print_all - prints a name as is
- * @format: name of the person
- *
- * Return: Nothing.
- */
+* print_all - function that print strings
+* @format: list of arguments c, i, f, *s
+* Return: void
+*/
 void print_all(const char * const format, ...)
 {
-	va_list p;
-	form forms[] = {
-		{'c', print_char},
-		{'i', print_int},
-		{'f', print_float},
-		{'s', print_string},
-		{'\0', NULL}
-	};
-	int i = 0, j = 0;
-	char *non_sep = "", *sep = ", ";
-
-	va_start(p, format);
-
-	while (format[i] != '\0')
-	{
-		j = 0;
-		while (forms[j].type != '\0')
-		{
-			if (forms[j].type == format[i])
-			{
-				printf("%s", non_sep);
-				forms[j].p(p);
-				non_sep = sep;
-			}
-			j++;
-		}
-		i++;
-	}
-	printf("\n");
-
-	va_end(p);
+unsigned int a = 0, b;
+char *string;
+va_list valist;
+while (format && format[a])
+{
+va_start(valist, format);
+while (format[a])
+{
+b = 1;
+switch (format[a++])
+{
+case 'c':
+printf("%c", va_arg(valist, int));
+break;
+case 'i':
+printf("%i", va_arg(valist, int));
+break;
+case 'f':
+printf("%f", va_arg(valist, double));
+break;
+case 's':
+string = va_arg(valist, char *);
+if (string == NULL)
+{
+string = "(nil)";
+}
+printf("%s", string);
+break;
+default:
+b = 0;
+break;
+}
+if (format[a] && b)
+{
+printf(", ");
+}
+}
+va_end(valist);
+}
+printf("\n");
 }
