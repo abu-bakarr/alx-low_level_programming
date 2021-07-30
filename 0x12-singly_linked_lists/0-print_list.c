0883158ba8b3eda028da71ffc5d5464b235b4e2d
abu-bakarr <abangura14@yahoo.com>
2021-07-30 05:43:52 +0000

    add print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ * print_list - function que print a linked list
+ * @h: pointer
+ * Return: retur of number of positions
+ */
+size_t print_list(const list_t *h)
+{
+unsigned int cont = 0;
+if (h)
+{
+while (h != NULL)
+{
+if (h[0].str != NULL)
+{
+printf("[%d] %s\n", h[0].len, h[0].str);
+}
+else
+{
+printf("[0] (nil)\n");
+}
+h = h[0].next;
+cont++;
+}
+}
+return (cont);
+}
