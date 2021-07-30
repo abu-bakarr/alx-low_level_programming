b72b629c61edad2a854330b324ef9810c82d88a5
abu-bakarr <abangura14@yahoo.com>
2021-07-30 05:52:07 +0000

    add advance hello holberton asm file

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,17 @@
+
+section function_printf
+	extern printf
+	section .to_print
+to_print: db "Hello, Holberton", 10
+
+	section .text
+	global main
+main:
+	push rbp
+	mov rdi, to_print
+	mov rax, 0
+	call printf
+	pop rbp
+	mov rax, 0
+	ret
+    
