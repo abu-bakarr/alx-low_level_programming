533dbc75a18c6eb816eb89de887bcbe5926c47fd
abu-bakarr <abangura14@yahoo.com>
2021-08-07 01:44:32 +0000

    flip bit

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * flip_bits - clear the bit
+ * @n: number to print
+ * @m: second number
+ * Return: 0
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int count = 0;
+	unsigned long int total = n ^ m;
+
+	while (total)
+	{
+		if (total & 1)
+			count += total & 1;
+		total >>= 1;
+	}
+	return (count);
+}
