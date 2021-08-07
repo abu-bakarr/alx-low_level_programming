5996dd618fb3e639d8dbe263889fa534a58d33fd
abu-bakarr <abangura14@yahoo.com>
2021-08-07 01:43:24 +0000

    clear bit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * clear_bit - clear the bit
+ * @n: number to print
+ * @index: index to obtain
+ * Return: 0
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	long int shift = 1;
+
+	if (!n)
+		return (-1);
+
+	if ((sizeof(long int) * 8) < index)
+		return (-1);
+
+	*n &= ~(shift <<= index);
+
+	return (1);
+}
