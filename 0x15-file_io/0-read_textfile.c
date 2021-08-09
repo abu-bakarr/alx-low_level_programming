72024888b2ceb2efd1bf43140e2d7f0b8edd08a9
abu-bakarr <abangura14@yahoo.com>
2021-08-09 21:43:44 +0000

    add header and read textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,43 +1,38 @@
 #include "holberton.h"
 /**
- * read_textfile - Function that reads a text file and prints it to the POSIX
- * standard output
- *
- * @filename: Pointer type char
- * @letters: Variable type size_t
- *
- * Return: The actual number of letters it could read and print
+*read_textfile - reads a text file and prints it to the POSIX standard output
+*@filename:char strinf of file name
+*@letters: size or number or letter to read.
+*Return: number of letter read and print or 0 If it is error.
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
+	int fdopen;
+	ssize_t readcount, writecount;
 	char *buffer;
-	ssize_t r_counter, w_counter;
 
 	if (filename == NULL)
 		return (0);
 
-	fd = open(filename, O_RDONLY);
-
-	if (fd == -1)
+	fdopen = open(filename, O_RDWR);
+	if (fdopen == -1)
 		return (0);
 
-	buffer = malloc(sizeof(buffer) * letters);
-
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
+	{
+		free(buffer);
+		return (0);
+	}
+	readcount = read(fdopen, buffer, letters);
+	if (readcount == -1)
 		return (0);
 
-	r_counter = read(fd, buffer, letters);
-
-	if (r_counter == -1)
+	writecount = write(STDOUT_FILENO, buffer, readcount);
+	if (writecount == -1 || readcount != writecount)
 		return (0);
-
-	w_counter = write(STDOUT_FILENO, buffer, r_counter);
-
-	if (w_counter == -1)
-		return (0);
-
-	close(fd);
 	free(buffer);
-	return (r_counter);
+
+	close(fdopen);
+	return (writecount);
 }
