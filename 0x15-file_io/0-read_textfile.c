adf4c680afc113671da8c6572a1278b5227df598
abu-bakarr <abangura14@yahoo.com>
2021-08-09 21:48:30 +0000

    add header and read textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,44 +1,38 @@
 #include "holberton.h"
 /**
- * read_textfile - function that reads a text file
- * and prints it to the standard output
- * @filename: name of the file
- * @letters: amount of letters to be read and printed
- * Return: (bytes2read)
- * actual number of letters it could read and print
- */
+*read_textfile - reads a text file and prints it to the POSIX standard output
+*@filename:char strinf of file name
+*@letters: size or number or letter to read.
+*Return: number of letter read and print or 0 If it is error.
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t bytes2read, bytes2write;
-	int f;
-	char *aux;
+	int fdopen;
+	ssize_t readcount, writecount;
+	char *buffer;
 
-	if (filename == NULL) /* checks if there is anything to read */
+	if (filename == NULL)
 		return (0);
 
-	f = open(filename, O_RDONLY); /* read the file only */
-
-	if (f == -1) /* if fail read, write, open == -1 */
+	fdopen = open(filename, O_RDWR);
+	if (fdopen == -1)
 		return (0);
 
-	aux = malloc(sizeof(char) * letters); /* space for \0 */
-	if (aux == NULL)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		free(buffer);
+		return (0);
+	}
+	readcount = read(fdopen, buffer, letters);
+	if (readcount == -1)
 		return (0);
 
-	bytes2read = read(f, aux, letters);
-	/* reading into f based on letters, of size aux */
-
-	if (bytes2read == -1)
+	writecount = write(STDOUT_FILENO, buffer, readcount);
+	if (writecount == -1 || readcount != writecount)
 		return (0);
+	free(buffer);
 
-	bytes2write =  write(STDOUT_FILENO, aux, bytes2read);
-	/* write to std out of buf based on bytes_read */
-
-	if (bytes2write == -1)
-		return (0);
-
-	free(aux);
-	close(f);
-
-	return (bytes2read);
+	close(fdopen);
+	return (writecount);
 }
