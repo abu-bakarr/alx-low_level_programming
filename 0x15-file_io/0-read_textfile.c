077bb7cc6e7dc376cc408f1cad7489a528d5bf21
abu-bakarr <abangura14@yahoo.com>
2021-08-11 17:33:14 +0000

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,44 +1,50 @@
 #include "holberton.h"
+
 /**
- * read_textfile - function that reads a text file
- * and prints it to the standard output
- * @filename: name of the file
- * @letters: amount of letters to be read and printed
- * Return: (bytes2read)
- * actual number of letters it could read and print
+ * read_textfile - read a certain size and prints to std output
+ * @filename: file to read from
+ * @letters: size to read
+ * Return: actual size read and printed
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t bytes2read, bytes2write;
-	int f;
-	char *aux;
+	int fd; /* file descriptor */
+	ssize_t n_read, n_wrote;
+	char *buffer;
 
-	if (filename == NULL) /* checks if there is anything to read */
+	if (filename == NULL)
 		return (0);
 
-	f = open(filename, O_RDONLY); /* read the file only */
-
-	if (f == -1) /* if fail read, write, open == -1 */
+	/* open */
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
 
-	aux = malloc(sizeof(char) * letters); /* space for \0 */
-	if (aux == NULL)
+	/* malloc buffer */
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 		return (0);
 
-	bytes2read = read(f, aux, letters);
-	/* reading into f based on letters, of size aux */
-
-	if (bytes2read == -1)
+	/* read */
+	n_read = read(fd, buffer, letters);
+	if (n_read == -1)
+	{
+		free(buffer);
+		close(fd);
 		return (0);
+	}
 
-	bytes2write =  write(STDOUT_FILENO, aux, bytes2read);
-	/* write to std out of buf based on bytes_read */
-
-	if (bytes2write == -1)
+	/* write */
+	n_wrote = write(STDOUT_FILENO, buffer, n_read);
+	if (n_wrote == -1)
+	{
+		free(buffer);
+		close(fd);
 		return (0);
+	}
 
-	free(aux);
-	close(f);
+	close(fd);
+	return (n_read);
 
-	return (bytes2read);
 }
