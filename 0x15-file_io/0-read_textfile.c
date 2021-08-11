f4f90eada9a9b9f8683bbb4c1f0e4766bf82fc94
abu-bakarr <abangura14@yahoo.com>
2021-08-11 17:41:27 +0000

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,34 +1,48 @@
-#include "holberton.h"
+#include "main.h"
+
 /**
- * read_textfile - function reads a text file and prints
- * it to the POSIX standard output.
- * @filename: char
- * @letters: list
- * Return: size of the list
- */
+ * read_textfile - a function that reads a text file and prints it
+ *                to POSIX standard output.
+ *
+ * @filename: is the file to read
+ * @letters: number of letters to read and print from file
+ *
+ * Return: 0 if it fails or actual number of letters it could
+ *         read and print
+*/
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int open_file, write_read;
-ssize_t sizeoflist;
-char *buffer = malloc(letters);
-if (buffer == NULL)
-{
-return (0);
-}
-if (filename == NULL)
-return (0);
-open_file = open(filename, O_RDONLY, 0600);
-if (open_file == -1)
-{
-return (0);
-}
-sizeoflist = read(open_file, buffer, letters);
-buffer[letters] = '\0';
-close(open_file);
-write_read = write(STDOUT_FILENO, buffer, sizeoflist);
-if (write_read == -1)
-{
-return (0);
-}
-return (sizeoflist);
+	int file;
+	ssize_t read_check, wcount;
+	char *buffer;
+
+	if (filename == NULL) /*check if file is present*/
+		return (0);
+
+	file = open(filename, O_RDONLY); /*open file*/
+
+	if (file == -1)
+		return (0);
+
+	/*get the size of buffer from number of letters*/
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	read_check = read(file, buffer, letters); /*read file*/
+	if (read_check == -1) /*check if read failed*/
+		return (0);
+
+	wcount = write(STDOUT_FILENO, buffer, read_check); /*write to POSIX*/
+	if (wcount == -1 || read_check != wcount) /*check if write failed*/
+		return (0);
+
+	free(buffer);
+
+	close(file); /*close file*/
+
+	return (wcount);
 }
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,15 +1,18 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
 
-#include <stdio.h>
-#include <stdlib.h>
+int _putchar(char c);
+
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+
 
 #endif
