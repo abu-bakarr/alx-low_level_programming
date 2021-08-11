4eba0f78a19e15930d09cc9b9fefa0a81ee2bfa7
abu-bakarr <abangura14@yahoo.com>
2021-08-11 17:30:50 +0000

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,44 +1,49 @@
 #include "holberton.h"
+
 /**
- * read_textfile - reads a file and write it to sdio
- *
- * @filename: SE
- * @letters: SE
- * Return: number of letters or -1
+ * read_textfile - function that reads a text file and prints it to the POSIX
+ * standard output
+ * @filename: Const char for name of file to be printed
+ * @letters: Size_t for number of bytes to be printed
+ * Return: Bytes read, 0 if error reading or writing to std output
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buffer;
-	int fileDescriptor;
-	ssize_t count, checkCount;
+	int fd;
+	ssize_t  n_read, n_write;
+	char *buf;
 
-	if (!filename)
+	buf = malloc(sizeof(char) * (letters + 1));
+
+	if (buf == NULL)
 		return (0);
-	buffer = malloc(sizeof(char) * letters);
-	if (!buffer)
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1 || filename == NULL)
 	{
+		free(buf);
 		return (0);
 	}
-	fileDescriptor = open(filename, O_RDONLY);
-	if (fileDescriptor == -1)
+
+	n_read = read(fd, buf, letters);
+
+	if (n_read < 0)
 	{
-		free(buffer);
+		free(buf);
 		return (0);
 	}
-	count = read(fileDescriptor, buffer, letters);
-	if (count == -1)
+
+	n_write = write(STDOUT_FILENO, buf, n_read);
+
+	if (n_write < 0)
 	{
-		free(buffer);
+		free(buf);
 		return (0);
 	}
-	/* same as write(1, file, number of characters)*/
-	checkCount = write(STDOUT_FILENO, buffer, count);
-	if (checkCount == -1 || (count != checkCount))
-	{
-		free(buffer);
-		return (0);
-	}
-	free(buffer);
-	close(fileDescriptor);
-	return (checkCount);
+
+	close(fd);
+	free(buf);
+	return (n_read);
 }
