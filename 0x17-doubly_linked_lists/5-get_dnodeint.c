9a5ff776b2de47025017eadfc60bf175ef797fff
abu-bakarr <abangura14@yahoo.com>
2021-09-17 08:42:45 +0000

    exercise 6

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - get node at given index
+ * @head: head of ddl list
+ * @index: node to get based on index
+ * Return: the address of found node or null if not found
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	size_t i = 0;
+
+	if (!head)
+		return (NULL);
+
+	for ( ; head && i < index; head = head->next, i++)
+		;
+	return (head);
+}
