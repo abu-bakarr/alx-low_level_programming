30404ee803dc241a2e67e0314b5571b9d782f2d7
abu-bakarr <abangura14@yahoo.com>
2021-09-17 08:39:00 +0000

    exercise 4

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * add_dnodeint_end - Add a new node at the start of the list
+ * @head: head of ddl list
+ * @n: member for new node to add
+ * Return: the address for new element or null if failed
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new, *old;
+
+	if (!head)
+		return (NULL);
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	if (!*head)
+	{
+		new->prev = NULL;
+		*head = new;
+		return (new);
+	}
+	old = *head;
+	for ( ; old->next; old = old->next)
+		;
+	old->next = new;
+	new->prev = old;
+	return (new);
+}
