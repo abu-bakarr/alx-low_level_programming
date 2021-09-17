60f4c2c89c23553e6f532bfe7c981b698680dcde
abu-bakarr <abangura14@yahoo.com>
2021-09-17 08:36:55 +0000

    exercise 2

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * dlistint_len - Short description, single line
+ * @h: head of ddl list
+ * Return: the number of nodes
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t size = 0;
+
+	for ( ; h; h = h->next, size++)
+		;
+	return (size);
+}
