74bf4cf754542dfd27a051874fc4c4e17911c782
abu-bakarr <abangura14@yahoo.com>
2021-09-17 08:35:47 +0000

    exercise 1

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,14 @@
+  #include "lists.h"
+/**
+ * print_dlistint - Short description, single line
+ * @h: head of ddl list
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t size = 0;
+
+	for (; h; h = h->next, size++)
+		printf("%d\n", h->n);
+	return (size);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This is a linked list added to low level programming
