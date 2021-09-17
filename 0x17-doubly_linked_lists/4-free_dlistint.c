11f3ace1717888505a0a32f4a6c311f0153255da
abu-bakarr <abangura14@yahoo.com>
2021-09-17 08:41:52 +0000

    exercise 5

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_dlistint - free linked list
+ * @head: head of ddl list
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *to_free = NULL;
+
+	while (head)
+	{
+		to_free = head->next;
+		free(head);
+		head = to_free;
+	}
+}
