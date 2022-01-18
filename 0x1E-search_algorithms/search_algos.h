09fa9a85ec945e8752946dd1c746e167b88df86c
abu-bakarr <abangura14@yahoo.com>
2022-01-18 08:49:42 +0000

    Added h file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,17 @@
+#ifndef _SEARCH_ALGO_H_
+#define _SEARCH_ALGO_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* Implementing linear search*/
+int linear_search(int *array, size_t size, int value);
+/* Implementing binary search*/
+int binary_search(int *array, size_t size, int value);
+/* Implementing Jump search*/
+int jump_search(int *array, size_t size, int value);
+/* Implementing interpolation search*/
+int interpolation_search(int *array, size_t size, int value);
+
+#endif
