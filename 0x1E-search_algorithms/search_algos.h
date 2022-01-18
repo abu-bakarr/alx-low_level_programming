52015b2cf93bef679489dda7b3409e6780903943
abu-bakarr <abangura14@yahoo.com>
2022-01-18 09:34:04 +0000

    Addedd exer adv

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -15,4 +15,23 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 /* Implementing expontial search*/
 int exponential_search(int *array, size_t size, int value);
+/* Implementing advanced_binary search*/
+int advanced_binary(int *array, size_t size, int value);
+
+/**
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @arr: 
+ */
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
+
+listint_t *jump_list(listint_t *list, size_t size, int value);
+
+
 #endif
