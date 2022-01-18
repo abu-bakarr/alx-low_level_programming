60f1edebe27ac4a65e5676fa3f5f2c5b8b3670cb
abu-bakarr <abangura14@yahoo.com>
2022-01-18 09:13:41 +0000

    Addedd exer 8

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,45 @@
+#include "search_algos.h"
+
+/**
+ * @array: list all the elements
+ * @size: size of array or list
+ * @value: matches with some value within array
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+	size_t skip = 0, x = 0;
+
+	if (array == NULL)
+		return (-1);
+
+	skip = sqrt(size);
+	while (i < size)
+	{
+		if (array[i] >= value)
+		{
+			x = (i - sqrt(size)) + 1;
+			printf("Value found between indexes [%lu] and [%lu]\n", x, i);
+			while (x <= i)
+			{
+				printf("Value checked array[%ld] = [%d]\n", x, array[x]);
+				if (array[x] == value)
+					return (x);
+				else if (x == i || array[x] > value)
+					return (-1);
+				x++;
+			}
+		}
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		i = skip;
+		skip = skip + sqrt(size);
+	}
+	x = (i - sqrt(size)) + 1;
+	printf("Value found between indexes [%lu] and [%lu]\n", x, i);
+	while (x < size)
+	{
+		printf("Value checked array[%ld] = [%d]\n", x, array[x]);
+		x++;
+	}
+	return (-1);
+}
